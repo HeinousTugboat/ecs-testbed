064df735edf0613cf5b11fea92186612e25e8cb3
HeinousTugboat <admin@heinousindustries.com>
2018-10-01 22:48:04 -0400

    adds variety of utility methods to CanvasManager

--- src/canvas.ts
+++ src/canvas.ts
@@ -30,13 +30,52 @@ export class CanvasManager {
   }
 
   clear() {
-    this.ctx.fillStyle = this.baseStyle;
     this.ctx.clearRect(0, 0, this.size.x, this.size.y);
+  }
+
+  dot(center: Vector, size: Vector) {
+    this.ctx.moveTo(center.x, center.y);
+    this.ctx.arc(center.x, center.y, size.magnitude, 0, 2 * Math.PI);
+  }
+
+  fill(style: CanvasFillStyle) {
+    this.setStyles();
+    this.ctx.fillStyle = style;
+    this.ctx.fill();
+  }
+
+  stroke(style: CanvasFillStyle, width: number = 1) {
+    this.setStyles();
+    this.ctx.strokeStyle = style;
+    this.ctx.lineWidth = width;
+    this.ctx.stroke();
+  }
+
+  line(start: Vector, end: Vector) {
+    this.ctx.moveTo(start.x, start.y);
+    this.ctx.lineTo(end.x, end.y);
   }
 
   rect(start: Vector, size: Vector, style: CanvasFillStyle) {
     this.ctx.fillStyle = style;
     this.ctx.fillRect(start.x, start.y, size.x, size.y);
+    // this.ctx.fillStyle = this.baseStyle;
+  }
+
+  setStyles() {
     this.ctx.fillStyle = this.baseStyle;
+    this.ctx.strokeStyle = this.baseStyle;
+    this.ctx.lineWidth = 0;
+  }
+
+  startPath() {
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, 0);
+    this.setStyles();
+  }
+
+  tick() {
+    this.clear();
+    this.startPath();
   }
 }
