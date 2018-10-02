5020a854dc8fd3193a63a6b791a1d8d0fad51ae6
HeinousTugboat <admin@heinousindustries.com>
2018-10-01 22:49:25 -0400

    breaks Render loop into two passes and moves stroke/fill calls to end of each pass

--- src/components/render.ts
+++ src/components/render.ts
@@ -1,15 +1,18 @@
 import { Component, System, Entity } from '../ecs';
 import { invalid, MutableVector as MVector, Vector, Color } from '../utils';
 import { CanvasManager } from '../canvas';
-
-
+import { VelocityComponent } from './velocity';
 
 export class RenderComponent extends Component {
   position: MVector = new MVector(0, 0);
-  size: Vector = new Vector(10, 10);
+  size: Vector = new Vector(1.5, 1.5);
   visible = true;
   color: Color = Color.BLACK;
 }
+
+let en: Entity | undefined;
+let render: RenderComponent | undefined;
+let velocity: VelocityComponent | undefined;
 
 export class RenderSystem extends System {
   constructor(private canvas: CanvasManager) {
@@ -17,17 +20,42 @@ export class RenderSystem extends System {
   }
   tick(dT: number) {
     this.canvas.clear();
+    // super.tick(dT);
 
-    super.tick(dT);
+    this.canvas.startPath();
+    this.entities.forEach(id => {
+      en = Entity.map.get(id);
+      if (en === undefined) {
+        return;
+      }
+      this.drawVelocity(en);
+    });
+
+    this.canvas.stroke(Color.LIGHT_BLUE.toString());
+    this.canvas.startPath();
+
+    this.entities.forEach(id => {
+      en = Entity.map.get(id);
+      if (en === undefined) {
+        return;
+      }
+      this.drawBody(en);
+    });
+    this.canvas.fill(Color.BLACK.toString());
   }
-  update(entity: Entity) {
-    const render = entity.get(RenderComponent);
+
+  drawBody(entity: Entity) {
+    render = entity.get(RenderComponent);
 
     if (invalid(render) || !render.visible) { return; }
-    this.canvas.rect(
-      render.size.scale(-0.5).add(render.position),
-      render.size,
-      render.color.toString()
-    );
+    this.canvas.dot(render.position, render.size);
+  }
+
+  drawVelocity(entity: Entity) {
+    render = entity.get(RenderComponent);
+    velocity = entity.get(VelocityComponent);
+
+    if (invalid(render) || !render.visible || invalid(velocity)) { return; }
+    this.canvas.line(render.position, render.position.toVector().add(velocity.velocity.toVector().scale(0.2)));
   }
 }
