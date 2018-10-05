27bdaec4924b66147875584724c809b4f91c71a4
HeinousTugboat <admin@heinousindustries.com>
2018-10-04 23:18:14 -0400

    changes velocity.accel to an MVector

--- src/components/velocity.ts
+++ src/components/velocity.ts
@@ -3,5 +3,5 @@ import { Vector, MutableVector } from '../utils';
 
 export class VelocityComponent extends Component {
   velocity: MutableVector = new MutableVector(0, 0);
-  acceleration: Vector = new Vector(0, 0);
+  acceleration: MutableVector = new MutableVector(0, 0);
 }
